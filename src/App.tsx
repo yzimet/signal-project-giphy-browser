6ec715af5e9c4f5f8becc33c3a4d9b154eb14558
Yaniv Zimet <yzimet@gmail.com>
2020-05-25 18:59:09 -0700

    display the Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import GifsGrid from './features/gifsGrid/GifsGrid';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Giphy Browser</header>
+      <GifsGrid />
     </div>
   );
 }
